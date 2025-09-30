adeaddb56db3a3ce7eb695d1fc10062805636f8b
Angelo2902 <181263820+Angelo2902@users.noreply.github.com>
2025-09-29 21:49:58 -0300

    aulas

---
+++ aulas/aula11/ex008.js
@@ -0,0 +1,6 @@
+var vel = 6
+console.log(`a velocidade do seu carro é de ${vel} por KM/h`)
+if (vel > 60) {
+    console.log('voce ultrapassou o limite de velocidade da via. MULTADO.')
+}
+console.log('dirija com cuidado adiante')
---
+++ aulas/aula11/ex009.js
@@ -0,0 +1,6 @@
+var país = 'EUA'
+if (país == 'Brasil') {
+    console.log('voce é brasileiro')
+}else{
+    console.log('voce é estrangeiro')
+}
