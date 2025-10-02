74a5c7a0c5e71f8840b1d63cf6886b92a5626e8c
Angelo2902 <181263820+Angelo2902@users.noreply.github.com>
2025-10-02 18:25:36 -0300

    exerciocio de verificador de idade
    aprendido a adicionar imagens com java e a modificar ele também usando java

---
+++ aulas/aula12ex/ex15/script.js
@@ -0,0 +1,52 @@
+function verificar() {
+    var data = new Date()
+    var ano = data.getFullYear()
+    var fano = document.getElementById('txtano')
+    var res = document.getElementById('res')
+    
+
+    if(fano.value.length == 0 || Number(fano.value) > ano){
+        window.alert('tente novamente')
+    }else{
+        var fsex = document.getElementsByName('radsex')
+        var idade = ano - Number(fano.value)
+        res.innerHTML = `idade calculada ${idade}`
+        var genero = ''
+
+        var img = document.createElement('img')//cria uma div usando javascript
+        img.setAttribute('id', 'foto')//coloca um atributo na div img, nesse caso o id dela é foto
+        res.style.textAlign = 'center'
+
+        if (fsex[0].checked){
+            genero = 'um Homem'
+
+            if (idade >= 0 && idade < 10){
+                img.setAttribute('src', '../imagens/bebem.png')//criança
+            }else if (idade < 21){
+                img.setAttribute('src', '../imagens/jovemm.png')//jovem
+            }else if (idade < 50){
+                img.setAttribute('src', '../imagens/tarde.png')//adulto
+            }else{
+                img.setAttribute('src', '../imagens/velhom.png')//idoso
+            }
+            
+        }else if (fsex[1].checked){
+            genero = 'uma Mulher'
+
+            if (idade >= 0 && idade < 10){
+                img.setAttribute('src', '../imagens/bebef.png') //criança
+            }else if (idade < 21){
+                img.setAttribute('src', '../imagens/jovemf.png') //jovem
+            }else if (idade < 50){
+                img.setAttribute('src', '../imagens/noite.png') //adulto
+            }else{
+                img.setAttribute('src', '../imagens/velhof.png') //idoso
+            }
+        
+            
+        }
+        res.innerHTML = `você é ${genero} com a idade de ${idade} anos`
+
+        res.appendChild(img)//adiciona um elemento apos todo o processo da function
+    }
+}
