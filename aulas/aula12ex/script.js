144ec0e885fdbb193c2fe87e4af7788d24e9a9fd
Angelo2902 <181263820+Angelo2902@users.noreply.github.com>
2025-10-01 22:34:08 -0300

    exercicio de modelo de horario em javascript

---
+++ aulas/aula12ex/script.js
@@ -0,0 +1,21 @@
+function carregar(){
+    var msg = window.document.getElementById('msg')
+    var img = window.document.getElementById('img')
+    var data = new Date()
+    //var hora = data.getHours()
+    var hora = 23
+    
+    msg.innerHTML = `Agora são ${hora} horas..`
+
+
+    if (hora > 0 && hora < 12){
+        img.src = 'imagens/dia.png'
+        document.body.style.backgroundColor = '#e2cd9f'
+    }else if (hora >= 12 && hora <= 22,33){
+        img.src = 'imagens/tarde.png'
+        document.body.style.backgroundColor = '#b9846f'
+    }else{
+        img.src = 'imagens/noite.png'
+        document.body.style.backgroundColor = '#515154'
+    }
+}
