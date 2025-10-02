866bf7b7deaf490a2c8378f1b48b77e56b5f707e
Angelo2902 <181263820+Angelo2902@users.noreply.github.com>
2025-10-01 21:24:33 -0300

    exercicios de java com datas e horas

---
+++ aulas/aula12/ex011.js
@@ -0,0 +1,11 @@
+var idade = 68
+console.log(`vc possui ${idade} anos, `)
+if (idade < 16) {
+    console.log('não vota')
+}else{
+if (idade<18 || idade > 65){
+    console.log('voto opcional')
+}else{
+    console.log('voto obrigatorio')
+}
+}
