9219e869f3ba8581e1cb0be45d158ed72196607f
Angelo2902 <181263820+Angelo2902@users.noreply.github.com>
2025-10-07 20:39:16 -0300

    tset

---
+++ aulas/aula14ex/exercicio16/scripit.js
@@ -0,0 +1,10 @@
+function carregar(){
+    var n1 = document.getElementById('iinicio')
+    var n2 = document.getElementById('ifim')
+    var n3  = document.getElementById('ipasso')
+
+}
+
+function somar(){
+    var 
+}
---
+++ testes/test.js
@@ -0,0 +1,7 @@
+var n1 = 1
+var n2 = 10
+var pas = 2
+var res = (n1 + pas)
+do{
+    console.log(res)
+}while(n1 <= n2)
