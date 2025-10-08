e2025e344670d843af40159fb922985b72c88013
Angelo2902 <181263820+Angelo2902@users.noreply.github.com>
2025-10-07 21:36:38 -0300

    resolução

--- aulas/aula14ex/exercicio16/scripit.js
+++ aulas/aula14ex/exercicio16/scripit.js
@@ -1,10 +1,35 @@
-function carregar(){
-    var n1 = document.getElementById('iinicio')
-    var n2 = document.getElementById('ifim')
-    var n3  = document.getElementById('ipasso')
-
-}
-
 function somar(){
-    var 
+    
+    var ini = document.getElementById('iinicio')
+    var fim = document.getElementById('ifim')
+    var pas  = document.getElementById('ipasso')
+    let res = document.getElementById('res')
+    
+
+    if (ini.value.length == 0 || fim.value.length == 0 || pas.value.length == 0){
+        window.alert('[ERRO], VOCE DEIXOU ALGO EM BRANCO')
+    } else {
+        res.innerHTML = 'contando: ... <br>'
+        let i = Number(ini.value)
+        let f = Number(fim.value)
+        let p = Number(pas.value)
+        if (p <= 0){
+            window.alert('passo invalido, sera considerado 1')
+            p = 1
+        }
+
+        if(i < f){
+            /*contagem crescente */
+            for(let c = i; c <= f; c += p){
+                res.innerHTML += ` ${c} `
+            }
+        }else{
+            /*contagem regressiva */
+            for(let c = i; c >= f; c -= p){
+                res.innerHTML += ` ${c} `
+            }
+        }
+    }
+
+    
 }
